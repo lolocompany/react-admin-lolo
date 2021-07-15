c47c80d42a4f910d8b42e64d102437eb4f2b5130
EvolveDeveloper <devatevolve@gmail.com>
2021-07-16 01:41:04 +0530

    updatee bundling flow
    add example app config flow

---
+++ example/src/App.js
@@ -0,0 +1,9 @@
+import {LoloAdmin} from '@inclusivedot/react-admin-lolo'
+
+function App() {
+  return (
+    <LoloAdmin />
+  );
+}
+
+export default App;
--- example/src/__tests__/react-admin-lolo/Admin.test.js
+++ example/src/__tests__/react-admin-lolo/Admin.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
-import {Admin} from '../Admin'
+import {Admin} from '@inclusivedot/react-admin-lolo'
 
 describe('test Admin suite', () => {
   test('render Admin Component', () => {
