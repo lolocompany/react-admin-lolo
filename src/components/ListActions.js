4f9b6d51ebb908ad464606cfbb33d85ced2d0bbb
Anders Wittfeldt <anders@lolo.company>
2023-01-13 21:45:15 +0100

    Add filterValues to CreateButton url

--- src/components/ListActions.js
+++ src/components/ListActions.js
@@ -2,12 +2,15 @@ import React, { cloneElement } from 'react';
 import * as ra from 'react-admin';
 import { ImportButton } from './';
 
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
 const ListActions = props => {
   const { className, exporter, filters, maxResults, hasCreate, ...rest } = props;
 
   const { currentSort, resource, displayedFilters, filterValues, basePath, showFilter, total } =
     ra.useListContext();
-
+    
   return (
     <ra.TopToolbar className={className} {...ra.sanitizeListRestProps(rest)}>
       {filters &&
@@ -20,7 +23,12 @@ const ListActions = props => {
         })}
       {hasCreate ? (
         <>
-          <ra.CreateButton basePath={basePath} />
+          <ra.CreateButton
+            to={{
+              pathname: `${basePath}/create`,
+              search: 'source=' + JSON.stringify(filterValues)
+            }}
+          />
           <ImportButton {...props} />
         </>
       ) : null}
